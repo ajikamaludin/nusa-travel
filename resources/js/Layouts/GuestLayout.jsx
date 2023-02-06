2535dc4fc08d804748a08a1c09a089052e001573
ajikamaludin <aji19kamaludin@gmail.com>
2023-02-07 00:45:27 +0700

    landing page nyicil

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -6,7 +6,7 @@ export default function Guest({ children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
-                <Link href="/">
+                <Link href={route('login')}>
                     <ApplicationLogo className="w-auto h-20 fill-current text-gray-500" />
                 </Link>
             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,162 @@
 
         <title>Laravel</title>
 
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+
         <!-- Styles -->
-        @vite(['resources/css/app.css'])
+        @vite(['resources/js/app.jsx'])
 
     </head>
-    <body class="antialiased text-2xl">
-        Hello World
+    <body class="antialiased">
+        <div class="w-full justify-center flex flex-col">
+            <div class="sticky top-0 z-50 bg-white">
+                <!-- Header -->
+                <div class="flex flex-row border-b-2 border-gray-100 pt-4 pb-2 px-2 lg:px-10">
+                    <div class="flex flex-row max-w-7xl mx-auto w-full justify-between items-center ">
+                        <div class="flex items-center">
+                            <img src="{{asset('logo-side.png')}}" class="h-10 w-auto"/>
+                        </div>
+                        <div class="flex flex-row gap-1 items-center">
+                            <div class="px-2.5 py-2.5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 flex flex-row gap-2">
+                                <img src="{{asset('images/flag_usa.png')}}" class="h-3 w-auto" alt="nation"/>
+                                <img src="{{asset('images/chevron_down.svg')}}" class="h-2 w-auto" alt="chevron-down"/>
+                            </div>
+                            <button type="button" class="px-5 py-2.5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">Sign Up</button>
+                            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+                        </div>
+                    </div>
+                </div>
+                <!-- Menu -->
+                <div class="flex flex-row gap-2 items-center border-b-2 border-gray-100 px-2 lg:px-16 overflow-auto">
+                    <div class="flex flex-row max-w-7xl mx-auto w-full items-center">
+                        <div class="my-1 px-4 py-2 text-sm font-medium text-gray-900 focus:outline-none bg-white hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">Home</div>
+                        <div class="my-1 px-4 py-2 text-sm font-medium text-gray-900 focus:outline-none bg-white hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">Tour Packages </div>
+                        <div class="my-1 px-4 py-2 text-sm font-medium text-gray-900 focus:outline-none bg-white hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">Fastboat</div>
+                        <div class="my-1 px-4 py-2 text-sm font-medium text-gray-900 focus:outline-none bg-white hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">Car Rentals</div>
+                        <div class="my-1 px-4 py-2 text-sm font-medium text-gray-900 focus:outline-none bg-white hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">Blog</div>
+                        <div class="my-1 px-4 py-2 text-sm font-medium text-gray-900 focus:outline-none bg-white hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">FAQ</div>
+                        <div class="my-1 px-4 py-2 text-sm font-medium text-gray-900 focus:outline-none bg-white hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">About Us</div>
+                    </div>
+                </div>
+            </div>
+            <!-- C -->
+            <div class="w-full mx-auto pt-1">
+                <div id="default-carousel" class="relative" data-carousel="static">
+                    <!-- Carousel wrapper -->
+                    <div class="relative h-56 overflow-hidden md:h-96">
+                        <!-- Item 1 -->
+                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                            <span class="absolute text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
+                            <img src="{{asset('images/1.jpg')}}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                        </div>
+                        <!-- Item 2 -->
+                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                            <img src="{{asset('images/2.jpg')}}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                        </div>
+                        <!-- Item 3 -->
+                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                            <img src="{{asset('images/3.jpg')}}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                        </div>
+                    </div>
+                    <!-- Slider indicators -->
+                    <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+                        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+                        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+                        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+                    </div>
+                    <!-- Slider controls -->
+                    <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                        <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                            <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
+                            <span class="sr-only">Previous</span>
+                        </span>
+                    </button>
+                    <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                        <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                            <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
+                            <span class="sr-only">Next</span>
+                        </span>
+                    </button>
+                </div>
+            </div>
+
+            <!-- popular packages -->
+            <div class="w-full max-w-7xl mx-auto">
+                <div class="flex flex-col w-full mx-auto pt-10 pb-5">
+                    <div class="text-2xl font-bold">Popular Tours</div>
+                    <div class="grid grid-cols-2 md:grid-cols-4 gap-4 py-2 px-2">
+                        @foreach([1,2,3,4] as $a)
+                        <div class="hover:-translate-y-2 ease-in duration-100 first-letter:max-w-sm bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+                            <a href="#">
+                                <img class="rounded-t-lg" src="{{asset('images/1.jpg')}}" alt="" />
+                            </a>
+                            <div class="p-5">
+                                <a href="#">
+                                    <h5 class="mb-1 tracking-tight text-gray-900 dark:text-white">Sunrise overlooking Borobudur at Punthuk Setumbu Hil...</h5>
+                                </a>
+                                <span class="mb-3 p-1 text-xs bg-gray-300 text-gray-700 dark:text-gray-400">Instant Confirmation</span>
+                                <div class="pt-5">
+                                    From 
+                                    <span class="text-base font-bold">Rp 562.000</span>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+
+            <!-- latest updates -->
+            <div class="w-full max-w-7xl mx-auto">
+                <div class="flex flex-col w-full mx-auto pt-10 pb-5">
+                    <div class="text-2xl font-bold">Lastest Updates</div>
+                    <div class="grid grid-cols-2 md:grid-cols-4 gap-4 py-2 px-2">
+                        @foreach([1,2,3,4] as $a)
+                        <div class="hover:-translate-y-2 ease-in duration-100 first-letter:max-w-sm bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+                            <a href="#">
+                                <img class="rounded-t-lg" src="{{asset('images/1.jpg')}}" alt="" />
+                            </a>
+                            <div class="p-5">
+                                <a href="#">
+                                    <h5 class="mb-1 tracking-tight text-gray-900 dark:text-white">Sunrise overlooking Borobudur at Punthuk Setumbu Hil...</h5>
+                                </a>
+                                <span class="mb-3 p-1 text-xs bg-gray-300 text-gray-700 dark:text-gray-400">Instant Confirmation</span>
+                                <div class="pt-5">
+                                    From 
+                                    <span class="text-base font-bold">Rp 562.000</span>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+
+            <!-- footer -->
+            <div class="w-full bg-gray-100">
+                <div class="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-4 w-full justify-between  py-5 px-16">
+                    <div>
+                        &copy; {{now()->format('Y')}} Nusa Travel. All Rights Reserved.
+                    </div>
+                    <div>
+                        <div class="font-bold pb-2">About</div>
+                        <div><a href="#">About Us</a></div>
+                        <div><a href="#">Blog</a></div>
+                    </div>
+                    <div>
+                        <div class="font-bold pb-2">Terms of use</div>
+                        <div>Terms of service</div>
+                        <div>Privacy Policy</div>
+                        <div>Cookie Policy</div>
+                        <div>Refund Policy</div>
+                        <div>Disclaimer</div>
+                    </div>
+                    <div>
+                        <div class="font-bold pb-2">Payment channels</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
