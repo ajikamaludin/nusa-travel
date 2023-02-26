882ab7080e4ba744a24bb40f5e0155b52e793f32
ajikamaludin <aji19kamaludin@gmail.com>
2023-02-26 17:31:24 +0700

    index package, next detail

--- resources/views/packages.blade.php
+++ resources/views/packages.blade.php
@@ -13,16 +13,16 @@
     <!-- Blog  -->
     <section class="w-full mx-auto max-w-7xl grid grid-cols-1 md:grid-cols-4 gap-4 py-2 md:py-5 px-1">
         @foreach($packages as $index => $package)
-        <div class="hover:-translate-y-2 ease-in duration-150 shadow bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 {{ $index == 0 ? 'col-span-2' : '' }}">
+        <div class="hover:-translate-y-2 ease-in duration-150 shadow bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 {{ $index == 0 ? 'md:col-span-2' : '' }}">
             <a href="{{ route('tour-packages.show', $package) }}">
-                <img class="rounded-t-lg" src="{{asset($package->cover_image)}}" alt="" />
+                <img class="rounded-t-lg {{ in_array($index, [1,2])  ? 'md:h-4/6' : ''}}" src="{{asset($package->cover_image)}}" alt="" />
             </a>
             <div class="p-3 md:p-5 space-y-1">
                 <a href="{{ route('tour-packages.show', $package) }}">
                     <h4 class="font-bold text-xl text-gray-900 dark:text-white line-clamp-1">{{ $package->title }}</h4>
                 </a>
                 <span class="mb-3 py-1 px-2 text-xs font-bold rounded bg-gray-200 text-gray-700 dark:text-gray-400">Instant Confirmation</span>
-                <div class="pt-5">
+                <div class="pt-3">
                     From
                     <span class="text-base font-bold">Rp. {{ number_format($package->price, '0', ',', '.') }}</span>
                 </div>
